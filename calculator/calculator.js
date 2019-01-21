3fb23486d6af07804c8c10ff193a8cb4fb934a14
Prakhar <prakhar@isglobalweb.com>
2019-01-21 18:33:27 +0530

    calculator commit

---
+++ calculator/calculator.js
@@ -0,0 +1,57 @@
+
+ var calArea = document.getElementById("cal-area");
+// console.log(calArea);
+
+// function calculation(e,operator){
+//     console.log(e.target,operator)
+// }
+class Calculator {
+
+    constructor() {
+        console.log("i am Calculator");
+    }
+
+}
+
+class Calculation extends Calculator {
+
+//    calArea = document.getElementById("cal-area");
+   
+   constructor() {
+       super();
+       console.log("i am Calculation");
+   }
+
+   data(e) {
+       
+        let val = !isNaN(e.path[0].innerText);
+       
+       if(val) {
+            console.log(e.path[0].innerText);
+            
+            if(calArea.value!=""){
+                calArea.value += e.path[0].innerText;
+                return 0;
+            }
+
+            calArea.value = e.path[0].innerText
+            console.log("after",calArea.value)
+
+       }
+
+   }
+
+   operator(evt,operator) {
+
+   }
+
+
+
+}
+
+calObj = new Calculation();
+
+
+
+
+
